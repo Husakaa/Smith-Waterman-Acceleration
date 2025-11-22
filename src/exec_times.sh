2fe4a4dbd15d806d649ebb749b14a76fcff5f7ec
Husakaa <hugosalascalderon@gmail.com>
2025-11-22 16:07:01 +0100

    fix: update exec_times.sh

--- src/exec_times.sh
+++ src/exec_times.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-for size in 32 64 128 256 512 1024; do
-    echo "Times for $size:" >> times.txt
-    for i in $(seq 1 3); do
-        ./Smith_Waterman_Cuda.exe resources/Homo_sapiens.fasta resources/Pan_troglodytes.fasta $size >> times.txt
-    done
-    echo "" >> times.txt # salto de linea
+rm ./times.txt
+for i in $(seq 1 10); do
+	echo "" >> times.txt
+	echo "Tiempo $i para Secuencial" >> times.txt
+	./Smith_Waterman_fasta.exe resources/Homo_sapiens.fasta resources/Pan_troglodytes.fasta >> times.txt
+	echo "" >> times.txt
+	echo "Tiempo $i para OpenMP" >> times.txt
+	./Smith_Waterman_Cpu.exe resources/Homo_sapiens.fasta resources/Pan_troglodytes.fasta >> times.txt
+    echo "Tiempo $i para CUDA" >> times.txt
+	./Smith_Waterman_Cuda.exe resources/Homo_sapiens.fasta resources/Pan_troglodytes.fasta 128 >> times.txt
+	echo "" >> times.txt
 done
